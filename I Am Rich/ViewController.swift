6f003958dba1e1f5ea2759cdb9a2d69fbd2f0564
Vam Damme <vam.damme@binus.ac.id>
2020-07-13 15:30:20 +0700

    initial commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Vam on 13/7/20.
+//  Copyright © 2020 Vam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
